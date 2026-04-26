6e7f9f277ed8926245c49bc0e3a81e97e021311d
xant1ko <semasavasa@gmail.com>
2026-04-26 17:22:31 +0700

    lesson of for

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 
-
 // const name = 'Анфиса'
 
 // let age = 83 
@@ -35,15 +34,45 @@
 
 
 // нужно вывести в консоль сумму длин всех строк
-const массивСтрок = ['j', 'q3r2222t', 'a4n', 'm1p9яячсчo', 'z8фывирпk2w', 'z8123фывирпk2wывыввыыв', 'h4j6yввввввввв']
+
+// const массивСтрок = ['j', 'q3r2222t', 'a4n', 'm1p9яячсчo', 'z8фывирпk2w', 'z8123фывирпk2wывыввыыв', 'h4j6yввввввввв']
+// let sum = 0;
+// for (let index=0; index<массивСтрок.length; index++) {
+//     sum += массивСтрок[index].length;    
+// }
+// console.log(sum);
+
+// for (let index=0; index<=100; index+=2) {
+//     console.log(index);
+// }
+
+
+const peoples = [
+    { name: "Алексей", age: 16, pants: "джинсы", intoxicationLevel: 1 },
+    { name: "Мария", age: 22, pants: "шорты", intoxicationLevel: 3 },
+    { name: "Дмитрий", age: 14, pants: "юбка", intoxicationLevel: 0 },
+    { name: "Елена", age: 19, pants: "джинсы", intoxicationLevel: 2 },
+    { name: "Иван", age: 17, pants: "шорты", intoxicationLevel: 4 },
+    { name: "Ольга", age: 21, pants: "юбка", intoxicationLevel: 1 },
+    { name: "Сергей", age: 18, pants: "джинсы", intoxicationLevel: 0 },
+    { name: "Анна", age: 20, pants: "шорты", intoxicationLevel: 3 },
+    { name: "Татьяна", age: 18, pants: "юбка", intoxicationLevel: 2 },
+    { name: "Павел", age: 16, pants: "джинсы", intoxicationLevel: 4 },
+    { name: "Кристина", age: 25, pants: "джинсы", intoxicationLevel: 2 },
+    { name: "Максим", age: 15, pants: "шорты", intoxicationLevel: 1 },
+    { name: "Юлия", age: 23, pants: "юбка", intoxicationLevel: 3 },
+    { name: "Андрей", age: 17, pants: "джинсы", intoxicationLevel: 0 },
+    { name: "Виктория", age: 19, pants: "шорты", intoxicationLevel: 4 }
+];
+
 let sum = 0;
-for (let index=0; index<массивСтрок.length; index++) {
-    sum += массивСтрок[index].length;
+
+for (let index=0; index<peoples.length; index++) {
+    console.log(peoples[index].name, peoples[index].age);
+    sum += peoples[index].age;
+   
+    
 }
-console.log(sum);
-
-
-
-
+console.log(sum/peoples.length);
 
 
